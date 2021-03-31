6fcdd0113bdb6bf2f1991db94e8c0f990f024420
kumaran-sivapunniyam <kumaran.sivapunniyam@gmail.com>
2021-03-31 09:20:00 -0500

    transformDeferred and transformDeferContextual

--- src/main/java/com/kani/reactor/defer/DeferUsingSubscriberContextlDemo.java
+++ src/main/java/com/kani/reactor/defer/DeferUsingSubscriberContextlDemo.java
@@ -4,19 +4,22 @@ import java.time.LocalDateTime;
 
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
+import reactor.util.context.Context;
 
 @Slf4j
-public class DeferContextualDemo {
+public class DeferUsingSubscriberContextlDemo {
 
-	public static void main(String[] args) throws InterruptedException {
+public static void main(String[] args) throws InterruptedException {
 		
 		log.info("Start Time:: {}", LocalDateTime.now().toString());
 		
+		@SuppressWarnings("deprecation")
 		Mono<String> greetingMono = 
 				Mono.deferContextual(ctx -> Mono.just("Greeting Time:: " + ctx.get("localTime") + " Hello " + ctx.get("message")))
 				.doOnNext(greeting -> log.info(greeting.toString()))
 				.map(String::toUpperCase)
-				.contextWrite(ctx -> ctx.put("message", "Karthi").put("localTime", LocalDateTime.now()));
+				//.contextWrite(ctx -> ctx.put("message", "Karthi").put("localTime", LocalDateTime.now()));
+				.subscriberContext(buildContext());
 		
 		Thread.sleep(5000);
 		greetingMono.doOnNext(s -> log.info(s.toString()))
@@ -25,5 +28,9 @@ public class DeferContextualDemo {
 		
 	}
 
-
+	private static Context buildContext() {
+		Context ctx = Context.of("message", "Karthi", "localTime", LocalDateTime.now());
+		return ctx;
+	}
+	
 }
---
+++ src/main/java/com/kani/reactor/transform/TransformDeferredDemo.java
@@ -0,0 +1,52 @@
+package com.kani.reactor.transform;
+
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
+@Slf4j
+public class TransformDeferredDemo {
+
+	public static void main(String[] args) {
+		
+		AtomicInteger ai = new AtomicInteger();
+		
+		
+		Function<Flux<String>, Flux<String>> filterAndMap = incomingFlux -> {
+			
+			int aiValue = ai.incrementAndGet();
+			log.info("ai={}", aiValue);
+			
+			Flux<String> transformedFlux = null;
+			
+			if( aiValue == 1) {
+				transformedFlux = incomingFlux.filter(name -> name.contains("Karthi")).map(String::toUpperCase);
+			}
+			else if( aiValue == 2) {
+				transformedFlux = incomingFlux.filter(name -> name.contains("Kumaran")).map(String::toUpperCase);
+			}
+			else {
+				transformedFlux = incomingFlux.filter(name -> name.contains(" Vignesh")).map(String::toUpperCase);
+			}
+			
+			return transformedFlux;
+		};
+				
+				
+
+		Flux<String> familyFlux = Flux.fromIterable(
+				Arrays.asList("Kavin Kumaran", "Nila Kumaran", "Vignesh Karthi", "Shankar Karthi", "Elle Vignesh"))
+				//.doOnNext(log::info)
+				.transformDeferred(filterAndMap);
+				
+		
+		
+		familyFlux.subscribe(name -> log.info("Subscriber Karthi to Composed MapAndFilter: {}",  name));
+		familyFlux.subscribe(name -> log.info("Subscriber Kumaran to Composed MapAndFilter: {}",  name));
+		familyFlux.subscribe(name -> log.info("Subscriber Vignesh to Composed MapAndFilter: {}",  name));
+	}
+	
+}
