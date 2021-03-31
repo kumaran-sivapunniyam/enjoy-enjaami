9e9846c9c2759293dd9957f11cdf2b1c286757c0
kumaran-sivapunniyam <kumaran.sivapunniyam@gmail.com>
2021-03-31 09:32:59 -0500

    transform vs transform with context

--- src/main/java/com/kani/reactor/transform/TransformDeferredWithContextDemo.java
+++ src/main/java/com/kani/reactor/transform/TransformDeferredWithContextDemo.java
@@ -1,6 +1,7 @@
 package com.kani.reactor.transform;
 
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
 import lombok.extern.slf4j.Slf4j;
@@ -9,52 +10,53 @@ import reactor.core.publisher.Flux;
 @Slf4j
 public class TransformDeferredWithContextDemo {
 
-	
-	
-public static void main(String[] args) {
+	public static void main(String[] args) {
+
+		AtomicInteger ai = new AtomicInteger();
 		
+		
+		//Same behavior is achieved, no difference between transform and transformDeferred
 		Function<Flux<String>, Flux<String>> filterAndMap = incomingFlux -> {
+
+			int aiValue = ai.incrementAndGet();
+			log.info("ai={}", aiValue);
 			
 			Flux<String> transformedFlux = null;
-			
+
 			transformedFlux = Flux.deferContextual(contextView -> {
-				
-				if(contextView.get("lastName").toString().equalsIgnoreCase("Karthi")) {
-					return incomingFlux.filter(name -> getLastName(name).equalsIgnoreCase("Karthi")).map(String::toUpperCase);
-				}
-				else if(contextView.get("lastName").toString().equalsIgnoreCase("Kumaran")) {
-					return incomingFlux.filter(name -> getLastName(name).equalsIgnoreCase("Kumaran")).map(String::toUpperCase);
-				}
-				else if(contextView.get("lastName").toString().equalsIgnoreCase("Vignesh")) {
-					return incomingFlux.filter(name -> getLastName(name).equalsIgnoreCase("Vignesh")).map(String::toUpperCase);
+
+				if (contextView.get("lastName").toString().equalsIgnoreCase("Karthi")) {
+					return incomingFlux.filter(name -> getLastName(name).equalsIgnoreCase("Karthi"))
+							.map(String::toUpperCase);
+				} else if (contextView.get("lastName").toString().equalsIgnoreCase("Kumaran")) {
+					return incomingFlux.filter(name -> getLastName(name).equalsIgnoreCase("Kumaran"))
+							.map(String::toUpperCase);
+				} else if (contextView.get("lastName").toString().equalsIgnoreCase("Vignesh")) {
+					return incomingFlux.filter(name -> getLastName(name).equalsIgnoreCase("Vignesh"))
+							.map(String::toUpperCase);
 				}
 				return Flux.empty();
 			});
-			
-			return transformedFlux;
-			
-		};
-				
-				
 
-		Flux<String> familyFlux = Flux.fromIterable(
-				Arrays.asList("Kavin Kumaran", "Nila Kumaran", "Vignesh Karthi", "Shankar Karthi", "Elle Vignesh"))
-				//.doOnNext(log::info)
-				.transform(filterAndMap);
-				
-		
-		familyFlux
-			.contextWrite(context -> context.put("lastName", "Karthi"))
-			.subscribe(name -> log.info("Subscriber Karthi to Composed MapAndFilter: {}",  name));
-		familyFlux
-			.contextWrite(context -> context.put("lastName", "Kumaran"))
-			.subscribe(name -> log.info("Subscriber Kumaran to Composed MapAndFilter: {}",  name));
-		familyFlux
-			.contextWrite(context -> context.put("lastName", "Vignesh"))
-			.subscribe(name -> log.info("Subscriber Vignesh to Composed MapAndFilter: {}",  name));
+			return transformedFlux;
+
+		};
+
+		Flux<String> familyFlux = Flux
+				.fromIterable(Arrays.asList("Kavin Kumaran", "Nila Kumaran", "Vignesh Karthi", "Shankar Karthi",
+						"Elle Vignesh"))
+				//.transform(filterAndMap)
+				.transformDeferred(filterAndMap);
+
+		familyFlux.contextWrite(context -> context.put("lastName", "Karthi"))
+				.subscribe(name -> log.info("Subscriber Karthi to Composed MapAndFilter: {}", name));
+		familyFlux.contextWrite(context -> context.put("lastName", "Kumaran"))
+				.subscribe(name -> log.info("Subscriber Kumaran to Composed MapAndFilter: {}", name));
+		familyFlux.contextWrite(context -> context.put("lastName", "Vignesh"))
+				.subscribe(name -> log.info("Subscriber Vignesh to Composed MapAndFilter: {}", name));
 	}
 
-private static String getLastName(String name) {
-	return name.split(" ")[1];
-}	
+	private static String getLastName(String name) {
+		return name.split(" ")[1];
+	}
 }
